468d67c5b4d62ecafaaf7fec33515bebaa71a5ce
DYuHLin <damianver2000@gmail.com>
2023-11-20 17:28:20 +0200

    Finished making the server as well as routing my html pages.

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const server = htttp.createServer((req, res) => {
-    
+    res.setHeader('content-type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            path += '/about.html';
+            res.statusCode = 301
+            break;
+        case '/contact-me':
+            path += 'contact-me.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+        
+    };
+
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        };
+    });
 });
 
 server.listen(8080, 'localhost', () => {
